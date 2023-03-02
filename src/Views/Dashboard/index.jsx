8e316aa64c2af12a9264d040eb634c120cd40441
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2023-03-02 17:04:17 +0530

    remove unnecessary files

--- src/Views/Dashboard/index.jsx
+++ src/Views/Dashboard/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useState } from 'react'
 import Header from '../../Components/Header'
 import TitleHeader from '../../Components/TitleHeader'
 import BootstrapTable from 'react-bootstrap-table-next'
@@ -6,21 +6,20 @@ import paginationFactory from 'react-bootstrap-table2-paginator'
 import orderup from '../../assets/images/order-up.svg'
 import orderdown from '../../assets/images/order-down.svg'
 import orderdefault from '../../assets/images/order-default.svg'
-import { useLocation } from 'react-router-dom'
+// import { useLocation } from 'react-router-dom'
 import Select from 'react-select'
-import { useDispatch, useSelector } from 'react-redux'
-import { deleteNorms, editSpecificNorms, getAllNormsAction } from '../../Actions/Admin/Norms/norms'
-import { Spinner } from 'react-bootstrap'
-import { useSnackbar } from 'react-notistack'
+// import { useDispatch, useSelector } from 'react-redux'
+// import { Spinner } from 'react-bootstrap'
+// import { useSnackbar } from 'react-notistack'
 import DeleteModal from '../../Components/DeleteModal/DeleteModal'
 import ActiveButton from '../../Shared/Component/ActiveButton'
 
 function NormsManagement () {
   // Constant
-  const dispatch = useDispatch()
-  const location = useLocation()
-  const token = localStorage.getItem('token')
-  const { enqueueSnackbar } = useSnackbar()
+  // const dispatch = useDispatch()
+  // const location = useLocation()
+  // const token = localStorage.getItem('token')
+  // const { enqueueSnackbar } = useSnackbar()
   const pagePerLimitArray = [
     { value: 10, label: '10' },
     { value: 20, label: '20' },
@@ -29,43 +28,43 @@ function NormsManagement () {
 
   // useState
   const [normsArray, setNormsArray] = useState([])
-  const [start, setStart] = useState(0)
+  const [, setStart] = useState(0)
   const [limit, setLimit] = useState(10)
-  const [order] = useState('asc')
-  const [sort] = useState('title')
-  const [search, setSearch] = useState('')
+  // const [order] = useState('asc')
+  // const [sort] = useState('title')
+  // const [search, setSearch] = useState('')
   const [id, setId] = useState([])
   const [show, setShow] = useState(false)
-  const [rowArray, setRowArray] = useState([])
+  // const [rowArray, setRowArray] = useState([])
   const [, setSortField] = useState('')
   const [, setSortOrder] = useState('asc')
   const [, setPage] = useState(1)
 
   // useSelector
-  const normsListData = useSelector((state) => state.norms.normsList)
-  const isNormsDeleted = useSelector((state) => state.norms.isDeleted)
-  const isNormsUpdated = useSelector((state) => state.norms.isNormsEdited)
-  const normsResMessage = useSelector((state) => state.norms.resMessage)
-  const count = useSelector((state) => state.norms.normsCount)
-  const isLoadning = useSelector((state) => state.norms.isLoadning)
+  // const normsListData = useSelector((state) => state.norms.normsList)
+  // const isNormsDeleted = useSelector((state) => state.norms.isDeleted)
+  // const isNormsUpdated = useSelector((state) => state.norms.isNormsEdited)
+  // const normsResMessage = useSelector((state) => state.norms.resMessage)
+  // const count = useSelector((state) => state.norms.normsCount)
+  // const isLoadning = useSelector((state) => state.norms.isLoadning)
 
   // previousProps
-  const previousProps = useRef({ normsListData, isNormsUpdated }).current
+  // const previousProps = useRef({ normsListData, isNormsUpdated }).current
 
   // useEffect for listing Data
-  useEffect(() => {
-    dispatch(getAllNormsAction(start, limit, sort, order, search, token))
-  }, [])
-  useEffect(() => {
-    if (previousProps?.normsListData !== normsListData) {
-      if (normsListData) {
-        setNormsArray(normsListData)
-      }
-    }
-    return () => {
-      previousProps.normsListData = normsListData
-    }
-  }, [normsListData])
+  // useEffect(() => {
+  //   dispatch(getAllNormsAction(start, limit, sort, order, search, token))
+  // }, [])
+  // useEffect(() => {
+  //   if (previousProps?.normsListData !== normsListData) {
+  //     if (normsListData) {
+  //       setNormsArray(normsListData)
+  //     }
+  //   }
+  //   return () => {
+  //     previousProps.normsListData = normsListData
+  //   }
+  // }, [normsListData])
 
   // Table Button : View, Edit, Delete
   const actionbutton = (row, cell) => {
@@ -80,11 +79,11 @@ function NormsManagement () {
   const handleClose = () => setShow(false)
   const handleDelete = (id) => {
     if (id) {
-      dispatch(deleteNorms({ id: [id] }, token))
+      // dispatch(deleteNorms({ id: [id] }, token))
     }
   }
   const selectRow = (row, isSelect, rowIndex) => {
-    setRowArray((oldArray) => [...oldArray, row.id])
+    // setRowArray((oldArray) => [...oldArray, row.id])
   }
 
   // Table Switch : Status on/off
@@ -99,10 +98,10 @@ function NormsManagement () {
 
   // Function to handle switch of table
   const handleChange = (e, id) => {
-    const data = {
-      id, isActive: e.target.checked ? 'y' : 'n', updateType: 'status'
-    }
-    dispatch(editSpecificNorms(data, token))
+    // const data = {
+    //   id, isActive: e.target.checked ? 'y' : 'n', updateType: 'status'
+    // }
+    // dispatch(editSpecificNorms(data, token))
     setNormsArray(normsArray.map(item => {
       if (item.id === id) {
         item.is_active = e.target.checked ? 'y' : 'n'
@@ -113,11 +112,11 @@ function NormsManagement () {
 
   // Search
   const handleCallback = (childData) => {
-    setSearch(childData)
+    // setSearch(childData)
     if (childData) {
-      dispatch(getAllNormsAction(0, limit, sort, order, childData, token))
+      // dispatch(getAllNormsAction(0, limit, sort, order, childData, token))
     } else {
-      dispatch(getAllNormsAction(0, limit, sort, order, '', token))
+      // dispatch(getAllNormsAction(0, limit, sort, order, '', token))
     }
   }
 
@@ -206,25 +205,25 @@ function NormsManagement () {
     setPage(page)
     setStart(limit * (page - 1) - 1 < 0 ? 0 : limit * (page - 1))
     if (page === 1) {
-      dispatch(getAllNormsAction(0, limit, sort, order, search, token))
+      // dispatch(getAllNormsAction(0, limit, sort, order, search, token))
     } else {
-      dispatch(
-        getAllNormsAction(
-          limit * (page - 1) - 1 < 0 ? 0 : limit * (page - 1),
-          limit,
-          sort,
-          order,
-          search,
-          token
-        )
-      )
+      // dispatch(
+      //   getAllNormsAction(
+      //     limit * (page - 1) - 1 < 0 ? 0 : limit * (page - 1),
+      //     limit,
+      //     sort,
+      //     order,
+      //     search,
+      //     token
+      //   )
+      // )
     }
   }
 
   // pagePerLimit
   const handlePagePerLimit = (e) => {
     setLimit(e.value)
-    dispatch(getAllNormsAction(start, e.value, sort, order, search, token))
+    // dispatch(getAllNormsAction(start, e.value, sort, order, search, token))
   }
 
   const options = {
@@ -232,7 +231,7 @@ function NormsManagement () {
     hideSizePerPage: true,
     hidePageListOnlyOnePage: false,
     alwaysShowAllBtns: true,
-    totalSize: count,
+    totalSize: 100,
     remote: { pagination: true },
     onPageChange
   }
@@ -250,64 +249,64 @@ function NormsManagement () {
       setSortField(sortField)
       setSortOrder(sortOrder)
       if (sortOrder) {
-        dispatch(
-          getAllNormsAction(start, limit, sort, sortOrder, search, token)
-        )
+        // dispatch(
+        //   // getAllNormsAction(start, limit, sort, sortOrder, search, token)
+        // )
       }
     }
   }
 
   // Notification for Delete
-  useEffect(() => {
-    if (previousProps?.isNormsDeleted !== isNormsDeleted) {
-      if (isNormsDeleted) {
-        setShow(false)
-        enqueueSnackbar(`${normsResMessage}`, {
-          variant: 'success',
-          hide: 2000,
-          autoHide: true
-        })
-        dispatch(getAllNormsAction(0, limit, sort, order, '', token))
-      } else if (isNormsDeleted === false) {
-        setShow(false)
-        enqueueSnackbar(`${normsResMessage}`, {
-          variant: 'error',
-          hide: 2000,
-          autoHide: true,
-          TransitionComponent: 'Fade'
-        })
-      }
-    }
-    return () => {
-      previousProps.isNormsDeleted = isNormsDeleted
-    }
-  }, [isNormsDeleted])
+  // useEffect(() => {
+  //   if (previousProps?.isNormsDeleted !== isNormsDeleted) {
+  //     if (isNormsDeleted) {
+  //       setShow(false)
+  //       enqueueSnackbar(`${normsResMessage}`, {
+  //         variant: 'success',
+  //         hide: 2000,
+  //         autoHide: true
+  //       })
+  //       // dispatch(getAllNormsAction(0, limit, sort, order, '', token))
+  //     } else if (isNormsDeleted === false) {
+  //       setShow(false)
+  //       enqueueSnackbar(`${normsResMessage}`, {
+  //         variant: 'error',
+  //         hide: 2000,
+  //         autoHide: true,
+  //         TransitionComponent: 'Fade'
+  //       })
+  //     }
+  //   }
+  //   return () => {
+  //     previousProps.isNormsDeleted = isNormsDeleted
+  //   }
+  // }, [isNormsDeleted])
 
   // Notification for Status
-  useEffect(() => {
-    if (previousProps?.isNormsUpdated !== isNormsUpdated) {
-      if (isNormsUpdated) {
-        setShow(false)
-        enqueueSnackbar(`${normsResMessage}`, {
-          variant: 'success',
-          hide: 2000,
-          autoHide: true
-        })
-        dispatch(getAllNormsAction(0, limit, sort, order, '', token))
-      } else if (isNormsUpdated === false) {
-        setShow(false)
-        enqueueSnackbar(`${normsResMessage}`, {
-          variant: 'error',
-          hide: 2000,
-          autoHide: true,
-          TransitionComponent: 'Fade'
-        })
-      }
-    }
-    return () => {
-      previousProps.isNormsUpdated = isNormsUpdated
-    }
-  }, [isNormsUpdated])
+  // useEffect(() => {
+  //   if (previousProps?.isNormsUpdated !== isNormsUpdated) {
+  //     if (isNormsUpdated) {
+  //       setShow(false)
+  //       enqueueSnackbar(`${normsResMessage}`, {
+  //         variant: 'success',
+  //         hide: 2000,
+  //         autoHide: true
+  //       })
+  //       // dispatch(getAllNormsAction(0, limit, sort, order, '', token))
+  //     } else if (isNormsUpdated === false) {
+  //       setShow(false)
+  //       enqueueSnackbar(`${normsResMessage}`, {
+  //         variant: 'error',
+  //         hide: 2000,
+  //         autoHide: true,
+  //         TransitionComponent: 'Fade'
+  //       })
+  //     }
+  //   }
+  //   return () => {
+  //     // previousProps.isNormsUpdated = isNormsUpdated
+  //   }
+  // }, [isNormsUpdated])
 
   return (
     <>
@@ -316,7 +315,7 @@ function NormsManagement () {
             name='Norms'
             title='Norms Management'
             url='add-new-Norms'
-            rowArray={rowArray}
+            rowArray={[]}
             location={location}
             showbuttons={true}
           />
@@ -350,7 +349,7 @@ function NormsManagement () {
                   onTableChange={handleTablechange}
                   responsive='md'
                   options={options}
-                  noDataIndication={ () => isLoadning ? <Spinner className='text-center' animation="border" /> : 'No data' }
+                  // noDataIndication={ () => isLoadning ? <Spinner className='text-center' animation="border" /> : 'No data' }
                 />
               </div>
           </div>
