b5a17ad013b862144ef51a8c5b5522ac41450ee6
HARDIK KANZARIYA <kanzariyahardik8511@gmail.com>
2023-03-06 18:28:22 +0530

    hardik: sign up page done

--- src/constants/apiEndpoints.js
+++ src/constants/apiEndpoints.js
@@ -1,3 +1,4 @@
 import { APP_URL, API_VERSION } from './apiConstants'
 
 export const API_LOGIN_ENDPOINT = `${APP_URL}/${API_VERSION}/users/login`
+export const API_SIGNUP_ENDPOINT = `${APP_URL}/${API_VERSION}/users/signup`
