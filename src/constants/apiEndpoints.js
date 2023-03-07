3ed320853ee5707a8b29c596761999ad59ccfa8d
HARDIK KANZARIYA <kanzariyahardik8511@gmail.com>
2023-03-07 10:49:23 +0530

    hardik: change in url

--- src/constants/apiEndpoints.js
+++ src/constants/apiEndpoints.js
@@ -1,4 +1,4 @@
-import { APP_URL, API_VERSION } from './apiConstants'
+import { APP_URL } from './apiConstants'
 
-export const API_LOGIN_ENDPOINT = `${APP_URL}/${API_VERSION}/users/login`
-export const API_SIGNUP_ENDPOINT = `${APP_URL}/${API_VERSION}/users/signup`
+export const API_LOGIN_ENDPOINT = `${APP_URL}/login`
+export const API_SIGNUP_ENDPOINT = `${APP_URL}/signup`
