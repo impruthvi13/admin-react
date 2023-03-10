64383a0dd29fa3f73ba1cf274f10561b4da50841
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2023-03-10 12:40:43 +0530

    store the users data in redux

--- src/Store/Actions/user.js
+++ src/Store/Actions/user.js
@@ -6,9 +6,8 @@ export async function addUser (userData) {
     const response = await axios.post(API_ADD_NEW_USER_ENDPOINT, data, { headers: { 'Content-Type': 'application/json', accept: 'application/json', Authorization: `Bearer ${token}` } })
     return response
   }
-
 export async function getUsers (userData) {
     const { offset, limit, token } = userData
     const response = await axios.get(API_LIST_USERS, { headers: { 'Content-Type': 'application/json', accept: 'application/json', Authorization: `Bearer ${token}` }, params: { limit, offset } })
-    return response
+    return response.data
 }
