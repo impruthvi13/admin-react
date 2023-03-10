074c23751db9d8994aec075951ba595eebcfe6b1
HARDIK KANZARIYA <kanzariyahardik8511@gmail.com>
2023-03-10 12:57:20 +0530

    hardik: change password api update

--- src/constants/apiEndpoints.js
+++ src/constants/apiEndpoints.js
@@ -5,3 +5,4 @@ export const API_SIGNUP_ENDPOINT = `${APP_URL}/signup`
 export const API_SIDEBAR_ENDPOINT = `${APP_URL}/admin/left-sidebar`
 export const API_ADD_NEW_USER_ENDPOINT = `${APP_URL}/admin/users`
 export const API_LIST_USERS = `${APP_URL}/admin/users`
+export const API_CHANGE_ADMIN_PASSWORD_ENDPOINT = `${APP_URL}/admin/change-password`
