4a8238096afe016596d830b78607f2eccf635f3a
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2023-04-27 18:12:59 +0530

    remove the View button from users list

--- src/Shared/Component/ActiveButton.js
+++ src/Shared/Component/ActiveButton.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import view from '../../assets/images/view-eye.svg'
 import edit from '../../assets/images/pencil-line.svg'
 import deletes from '../../assets/images/delete-bin-line.svg'
@@ -7,6 +7,21 @@ import deletes from '../../assets/images/delete-bin-line.svg'
 import PropTypes from 'prop-types'
 
 function ActiveButton ({ id, handleShow, slug, viewlink, editlink }) {
+  const location = useLocation()
+
+  if (location.pathname === '/admin/users') {
+    return <div className="button-box">
+      <Link to={`${editlink}/${id}`}>
+        <button className="action-btns light-blue-bg" type="button">
+          <img src={edit} alt="" /> Edit
+        </button>
+      </Link>
+      <button className="action-btns light-red-bg" onClick={() => handleShow(id)} type="button">
+        <img src={deletes} alt="" /> Delete
+      </button>
+    </div>
+  }
+
   return <div className="button-box">
     <Link to={`${viewlink}/${id}`}>
       <button className="action-btns green-bg" type="button">
